ae66cb2eafb6282c7a7d6c09d9215d611eaf6033
lewis94 <lewisrosario94@gmail.com>
2022-01-18 11:13:52 -0400

    Se agrego el template y el fragment para visualizar los calculos en el index html

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ let isr = 0
 const meses = 12
 let salarioAnual
 
+const divTemplate = document.getElementById('template-div')
+const template = document.getElementById('template-calculo').content
+const fragment = document.createDocumentFragment()
+
 const c = console.log
 
+c(template)
 
 
 //Funcion Calculo ISR segun DGI actualizado en fecha Enero/17/2022...
@@ -67,29 +72,40 @@ const calcularAFP = (sMensual) =>{
 //Funcion Calculo Total Retenciopnes...
 const calcularTotalRet = ()=>{
     let ttRet = calcularAFP(Number(inputCalcular.value)) + calcularSFS(Number(inputCalcular.value)) + calcularISR(salarioAnual)
-    c(`Total retenciones : ${ttRet}`)
-    
+  //  c(`Total retenciones : ${ttRet}`)
     return ttRet
 }
 //Funcion Calculo Salario Neto Mensual...
 const calcularSNetoMensual = (salarioMensual) =>{
     let ttSalarioNetoMensual = salarioMensual - calcularTotalRet()
-    c(`Salario Neto Mensual ${ttSalarioNetoMensual}`)
+   // c(`Salario Neto Mensual ${ttSalarioNetoMensual}`)
     return ttSalarioNetoMensual
 }
 //Funcion Calculo Salario Neto Quincenal...
 const calcularSNetoQuincenal = (salarioMensual) =>{
     let ttsNetoQuincenal = calcularSNetoMensual(salarioMensual) / 2
-    c(`Salario Neto Quincenal ${ttsNetoQuincenal}`)
+   // c(`Salario Neto Quincenal ${ttsNetoQuincenal}`)
     return ttsNetoQuincenal
 }
-
 
 btnCalcular.addEventListener('click',(e) => {
 
     salarioAnual = Number(inputCalcular.value) * meses
-    c(`Salario mensual: ${inputCalcular.value} Salario anual: ${salarioAnual}`)
-    calcularSNetoQuincenal(Number(inputCalcular.value))
+   // c(`Salario mensual: ${inputCalcular.value} Salario anual: ${salarioAnual}`)
+   // calcularSNetoQuincenal(Number(inputCalcular.value))
+
+    template.querySelector('#sfs').textContent = `Seguro Familiar de Salud (SFS): ${calcularSFS(Number(inputCalcular.value))}` 
+    template.querySelector('#afp').textContent = `Administradora de Fondo de Pensiones (AFP): ${calcularAFP(Number(inputCalcular.value))}`
+    template.querySelector('#isr').textContent = `Impuesto Sobre la Renta (ISR): ${calcularISR(salarioAnual)}`
+    template.querySelector('#ttr').textContent = `Total Retenciones: ${calcularTotalRet()}`
+    template.querySelector('#snm').textContent = `Salario Neto Mensual: ${calcularSNetoMensual(Number(inputCalcular.value))}`
+    template.querySelector('#snq').textContent = `Salario Neto Quincenal:${calcularSNetoQuincenal(Number(inputCalcular.value))}`
+
+
+    const clone = template.cloneNode(true)
+    fragment.appendChild(clone)
+
+    divTemplate.appendChild(fragment)
 
    e.preventDefault()
 
